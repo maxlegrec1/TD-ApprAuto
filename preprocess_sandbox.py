3020168208520cdb57e6c027e20282c20f8d340f
2Mariousse <themarius.boucaut@gmail.com>
2024-10-07 16:06:02 +0200

    add(preprocess_sandbox.py) for colleagues

---
+++ preprocess_sandbox.py
@@ -0,0 +1,32 @@
+from data import COLUMNS
+"""
+current file is only used to inspect data and find patterns throught the data, it should be used in actual preprocessing 
+"""
+    
+if __name__ == "__main__":
+    data = pd.read_csv("welddb/welddb.data", delim_whitespace=True, header=None,names=COLUMNS)
+    #N stands for missing data:
+    data=data.replace("N",pd.NA)
+
+    #first idea is to show missing data according to type of weld
+    weld_types=data["Type of weld"].unique()
+    
+    #MMA and ShMA are the same
+    data["Type of weld"]=data["Type of weld"].replace("ShMA","MMA")
+
+    #Now trying to find which columns are useless for each weld type
+    NO_DATA={} # weld_type:list[feature] where feature is never given for weld_type instance
+    missing_values_by_weld_type=pd.DataFrame()
+    for weld_type in weld_types:
+        df=data[data["Type of weld"]==weld_type]
+        missing_values=df.isnull().mean()
+        missing_features=missing_values[missing_values==1].index
+        NO_DATA[weld_type]=missing_features
+
+        missing_values_by_weld_type[weld_type]=missing_values
+    
+    # we should be checking at least std, maybe min max etc
+    std=missing_values_by_weld_type.std(axis=1)
+
+    breakpoint()
+
