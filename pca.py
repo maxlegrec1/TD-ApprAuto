7f50b9e701c85e5157402eb2c1fc434f9bc5baf9
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-10-07 08:39:22 +0200

    feature importance

--- pca.py
+++ pca.py
@@ -9,12 +9,12 @@ def pca(X_train: pd.DataFrame, X_test: pd.DataFrame | None = None, n_components:
             return X_train
         return X_train, X_test
     
-    scaler = StandardScaler()
+    # scaler = StandardScaler()
     pca = PCA(n_components=n_components)
 
-    X_train_pca = pd.DataFrame(pca.fit_transform(scaler.fit_transform(X_train)))
+    X_train_pca = pd.DataFrame(pca.fit_transform(X_train))
     if X_test is None:
         return X_train_pca
 
-    X_test_pca = pd.DataFrame(pca.transform(scaler.transform(X_test)))
+    X_test_pca = pd.DataFrame(pca.transform(X_test))
     return X_train_pca, X_test_pca
